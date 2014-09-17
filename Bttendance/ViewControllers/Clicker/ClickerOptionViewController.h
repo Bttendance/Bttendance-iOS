0b946cafed7bf598ebfc84360f22d3eae066c1b9
TheFinestArtist <contact@thefinestartist.com>
2014-09-17 12:18:50 +0900

    update clicker option

---
+++ Bttendance/ViewControllers/Clicker/ClickerOptionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ClickerOptionViewController.h
+//  bttendance
+//
+//  Created by TheFinestArtist on 2014. 9. 17..
+//  Copyright (c) 2014년 Bttendance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClickerOptionViewController : TableViewController
+
+@end
---
+++ Bttendance/ViewControllers/Clicker/ClickerOptionViewController.m
@@ -0,0 +1,13 @@
+//
+//  ClickerOptionViewController.m
+//  bttendance
+//
+//  Created by TheFinestArtist on 2014. 9. 17..
+//  Copyright (c) 2014년 Bttendance. All rights reserved.
+//
+
+#import "ClickerOptionViewController.h"
+
+@implementation ClickerOptionViewController
+
+@end
